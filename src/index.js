6f0de6a196e8edc6ad1f3d4f2f23c51253cef28f
Elizaveta Sokolovskaya <epogrosheva@gmail.com>
2020-02-21 14:28:21 +0300

    feat: implement typical-arrays-problems function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    return Math.min(...array);
+};
+
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    }
+    return Math.max(...array);
+
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+        avg += array[i];
+    }
+
+    return avg / (array.length);
 }
